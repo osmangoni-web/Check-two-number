042d8d44d7f59d60082caddd8232b4ac93cf653a
Md. Osman Goni <67469321+osmangoni-web@users.noreply.github.com>
2023-11-09 22:43:23 +0600

    Add files via upload

---
+++ Greater than or less than.py
@@ -0,0 +1,9 @@
+#Greater than or less than
+
+x = input("Enter the first Value :")
+y = input("Enter the Second value :")
+
+if x > y:
+    print("The first Value is great number")
+else:
+    print(" The Second value is great number")
